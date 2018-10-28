af6a7bb42f737c4e4013c78b0f6acf2c2300e599
bhrobot <brandonhaberman@gmail.com>
2018-10-28 11:25:14 -0400

    fixed the sql table for user_clicks

--- sql_creation.sql
+++ sql_creation.sql
@@ -164,15 +164,15 @@
 		PRIMARY KEY (SearchID),
 		FOREIGN KEY (UserID) REFERENCES phprbac_users(UserID));
 
-	CREATE TABLE IF NOT EXISTS User_Clicks (
-		ClickID int(11) NOT NULL auto_increment,
-		UserID int(11),
-		FromSearchID int(11),
-		SpaceID int(11),
-		FromLatitude real,
-		FromLongitude real,
-		FromIP varchar(15),
-		ClickTime timestamp DEFAULT CURRENT_TIMESTAMP,
-		PRIMARY KEY (ClickID),
-		FOREIGN KEY (UserID) REFERENCES phprbac_users(UserID),
-		FOREIGN KEY (SearchID) REFERENCES FromSearchID(SearchID));
+		CREATE TABLE IF NOT EXISTS User_Clicks (
+			ClickID int(11) NOT NULL auto_increment,
+			UserID int(11),
+			FromSearchID int(11),
+			SpaceID int(11),
+			FromLatitude real,
+			FromLongitude real,
+			FromIP varchar(15),
+			ClickTime timestamp DEFAULT CURRENT_TIMESTAMP,
+			PRIMARY KEY (ClickID),
+			FOREIGN KEY (UserID) REFERENCES phprbac_users(UserID),
+			FOREIGN KEY (FromSearchID) REFERENCES Search_Log(SearchID));
