948275c9f45194e7471c7801e4907e1459ff57bd
Brandon Haberman <brandonhaberman@gmail.com>
2018-11-06 21:18:11 -0500

    small sql db fix

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,16 @@
 <?php session_start();
 
+// // set the credentials for MySQL
+// $dbhost = "mydb.itap.purdue.edu"; // this will ususally be 'localhost', but can sometimes differ
+// $dbname = "bhaberma"; // the name of the database that you are going to use for this project
+// $dbuser = "bhaberma"; // the username that you created, or were given, to access your database
+// $dbpass = "Haberman2o!8"; // the password that you created, or were given, to access your database
+
 // set the credentials for MySQL
-$dbhost = "mydb.itap.purdue.edu"; // this will ususally be 'localhost', but can sometimes differ
-$dbname = "bhaberma"; // the name of the database that you are going to use for this project
-$dbuser = "bhaberma"; // the username that you created, or were given, to access your database
-$dbpass = "Haberman2o!8"; // the password that you created, or were given, to access your database
+$dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
+$dbname = "ie332group11"; // the name of the database that you are going to use for this project
+$dbuser = "root"; // the username that you created, or were given, to access your database
+$dbpass = ""; // the password that you created, or were given, to access your database
 
 // creates the connection to the database
 $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
