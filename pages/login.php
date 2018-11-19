b11d290243bbfb4d158bd31f50b9aa310df8c45d
Brandon Haberman <brandonhaberman@gmail.com>
2018-11-19 16:45:50 -0500

    see pull request 31
    https://github.com/bStrangerman/ie332group11/pull/31

--- pages/login.php
+++ pages/login.php
@@ -93,7 +93,7 @@ View settings for more info. -->
 					<span class="border"></span>
 				</label>
 				<div class="formDiv">
-					<input type="submit" name="reg_user" value="Register"<?php if($email == NULL || $username == NULL || $psswd_insert == NULL)echo Please Complete Form?>>
+					<input type="submit" name="reg_user" value="Register">
 				</div>
 				<footer>
 					<p class="registerBtn">Back to login</p>
