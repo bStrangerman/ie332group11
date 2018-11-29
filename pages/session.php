0ef63ac9a6108fa7ebc25c7f1450554bb4fe11f8
Brandon Haberman <brandonhaberman@gmail.com>
2018-11-29 16:13:41 -0500

    Update session.php

--- pages/session.php
+++ pages/session.php
@@ -1,5 +1,32 @@
 <?php
 require_once "../includes/main.php";
+
+echo '$ip=$_SERVER["HTTP_CLIENT_IP"]: ' . $ip=$_SERVER['HTTP_CLIENT_IP'] . "<br>";
+echo '$ip=$_SERVER["HTTP_X_FORWARDED_FOR"]: ' . $ip=$_SERVER['HTTP_X_FORWARDED_FOR'] . "<br>";
+echo '$ip=$_SERVER["REMOTE_ADDR"]: ' . $ip=$_SERVER['REMOTE_ADDR'] . "<br>";
+
+
+function get_client_ip() {
+    $ipaddress = '';
+    if ($_SERVER['HTTP_CLIENT_IP'])
+        $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+    else if($_SERVER['HTTP_X_FORWARDED_FOR'])
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    else if($_SERVER['HTTP_X_FORWARDED'])
+        $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+    else if($_SERVER['HTTP_FORWARDED_FOR'])
+        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+    else if($_SERVER['HTTP_FORWARDED'])
+        $ipaddress = $_SERVER['HTTP_FORWARDED'];
+    else if($_SERVER['REMOTE_ADDR'])
+        $ipaddress = $_SERVER['REMOTE_ADDR'];
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+$ip = get_client_ip();
+echo "IP: " . $ip;
+
 array_print($_COOKIE);
 
 
