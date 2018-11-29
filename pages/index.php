c7fdfe51a4a2af6eed4715517aaea77f88fe0ef5
AuBohlin <abohlin@purdue.edu>
2018-11-29 13:32:51 -0500

    Gets IP

--- pages/index.php
+++ pages/index.php
@@ -11,6 +11,25 @@ else {
   $warehouseRole = 0;
   $LesseeRole = 0;
 }
+
+//Grab REAL IP (not $_SERVER[‘REMOTE_ADDR’])
+function getRealIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
 ?>
 
 <!doctype html>
