5dff4d8a242326f399b9e194e5936ecf0872f471
Brandon Haberman <brandonhaberman@gmail.com>
2018-11-30 12:58:37 -0500

    BONUS POINTS Lessee Account information page is working
    allows users to change account info

--- pages/lesseeProfile.php
+++ pages/lesseeProfile.php
@@ -15,9 +15,6 @@ else{
   $UserID = $_SESSION['UserID'];
 }
 
-// print the header
-require_once "../layouts/Calssimax/header.php";
-
 // get user info
 $sql = "SELECT *
         FROM phprbac_users
@@ -25,32 +22,67 @@ $sql = "SELECT *
 $result = $conn -> query($sql);
 while($LesseeInfo[]=mysqli_fetch_array($result));
 
-?>
-<!--==================================
+
+if(isset($_POST['editing'])){
+  if($_POST['editing'] == 'profile'){
+    $firstname = clean($_POST['first-name']);
+    $lastname = clean($_POST['last-name']);
+    $email = clean($_POST['Email']);
+    $phonenumber = clean($_POST['PhoneNumber']);
+    $company = clean($_POST['Company']);
+    $sql = "UPDATE phprbac_users
+            SET FirstName = '$firstname', LastName = '$lastname', Email = '$email', PhoneNumber = $phonenumber, Company = '$company'
+            WHERE UserID = $UserID";
+    if($conn -> query($sql) === TRUE){
+      $_SESSION['message'] = "Success!";
+      header("Refresh: 0.1");
+      $_SESSION['message'] = "Success!";
+    }
+  }
+  if($_POST['editing'] == 'password'){
+    if(md5(clean($_POST['current-password'])) == $LesseeInfo[0]['Password']){
+      $newPassword = md5(clean($_POST['new-password']));
+      $sql = "UPDATE phprbac_users
+              SET Password = '$newPassword'
+              WHERE UserID = $UserID";
+      if($conn -> query($sql) === TRUE){
+        $_SESSION['message'] = "Success!";
+        header("Refresh: 0.1");
+        $_SESSION['message'] = "Success!";
+      }
+    }
+  }
+}
+
+
+// print the header
+require_once "../layouts/Calssimax/header.php";
+
+
+?><!--==================================
 =            User Profile            =
 ===================================-->
-<section class="dashboard section">
-	<!-- Container Start -->
+
+<section class="user-profile section">
 	<div class="container">
-		<!-- Row Start -->
 		<div class="row">
 			<div class="col-md-10 offset-md-1 col-lg-4 offset-lg-0">
 				<div class="sidebar">
 					<!-- User Widget -->
 					<div class="widget user-dashboard-profile">
-						<!-- User Image -->
+            <!-- User Image -->
 						<!-- <div class="profile-thumb">
 							<img src="images/user/user-thumb.jpg" alt="" class="rounded-circle">
 						</div> -->
 						<!-- User Name -->
 						<h5 class="text-center"><?php echo $LesseeInfo[0]['FirstName'] . " " . $LesseeInfo[0]['LastName']; ?></h5>
 						<p>Joined <?php echo date("F d, Y" , strtotime($LesseeInfo[0]['CreationDate'])); ?></p>
-						<a href="lesseeProfile.php" class="btn btn-main-sm">Edit Profile</a>
+						<a href="user-profile.html" class="btn btn-main-sm">Edit Profile</a>
 					</div>
 					<!-- Dashboard Links -->
-					<div class="widget user-dashboard-menu">
+          <div class="widget user-dashboard-menu">
 						<ul>
-							<li class="active" ><a href=""><i class="fa fa-user"></i> My Contracts</a></li>
+							<li class="" ><a href="lesseeAccount.php"><i class="fa fa-user"></i> My Contracts</a></li>
 							<!-- <li><a href=""><i class="fa fa-bookmark-o"></i> Favourite Ads <span>5</span></a></li>
 							<li><a href=""><i class="fa fa-file-archive-o"></i>Archived Ads <span>12</span></a></li>
 							<li><a href=""><i class="fa fa-bolt"></i> Pending Approval<span>23</span></a></li> -->
@@ -61,93 +93,80 @@ while($LesseeInfo[]=mysqli_fetch_array($result));
 				</div>
 			</div>
 			<div class="col-md-10 offset-md-1 col-lg-8 offset-lg-0">
-				<!-- Recently Favorited -->
-				<div class="widget dashboard-container my-adslist">
-					<h3 class="widget-header">My Contracts</h3>
-          <?php
-          $sql = "SELECT *
-                  FROM Contracts
-                  LEFT JOIN Spaces
-                  ON Spaces.SpaceID = Contracts.SpaceID
-                  LEFT JOIN Warehouses
-                  ON Warehouses.WarehouseID = Spaces.WarehouseID
-                  LEFT JOIN Contract_Status
-                  ON Contract_Status.ContractID = Contracts.ContractID
-                  LEFT JOIN Status
-                  ON Status.StatusID = Contract_Status.StatusID
-                  WHERE Contracts.LesseeID = $UserID";
-          $result = $conn -> query($sql);?>
-					<table class="table table-responsive product-dashboard-table">
-						<thead>
-							<tr>
-								<th>Location</th>
-								<th>Contract Info</th>
-								<th class="text-center">Status</th>
-								<!-- <th class="text-center">Action</th> -->
-							</tr>
-						</thead>
-						<tbody>
-              <?php
-              while($row = $result -> fetch_assoc()){
-                $warehouseID = $row['WarehouseID'];
-
-                $pictureSQL = "SELECT FileName
-                               FROM Pictures
-                               LEFT JOIN Warehouse_Pictures
-                               ON Warehouse_Pictures.PictureID = Pictures.PictureID
-                               WHERE Warehouse_Pictures.WarehouseID = $warehouseID
-                               LIMIT 1";
-                $pictureResult = $conn -> query($pictureSQL);
-
-                echo "<tr>";
-
-                while($picture = $pictureResult -> fetch_assoc()){ ?>
-                  <td class="product-thumb">
-  									<img width="auto" height="100px" src="../pictures/<?php echo $picture['FileName']; ?>" alt="<?php echo $picture['FileName']; ?>"></td>
-                <?php }?>
-
-								<td class="product-details">
-									<h3 class="title"><?php echo $row['Address']; ?></h3>
-									<span class="add-id"><strong>Contract ID:</strong> <?php echo $row['ContractID']; ?></span>
-									<span><strong>Updated on: </strong><time><?php echo $row['StatusTime']; ?></time> </span>
-									<!-- <span class="status active"><strong>Status</strong><?php echo $row['StatusName']; ?></span> -->
-									<span class="location"><strong>Location</strong><?php echo $row['City'] . ", " . $row['State']; ?></span>
-								</td>
-								<td class="product-category"><span style="color: <?php echo ($row['StatusName'] == 'Denied') ? 'red' : (($row['StatusName'] == 'Approved') ? 'green' : 'orange') ?>" class="categories"><?php echo $row['StatusName']; ?></span></td>
-								<!-- <td class="action" data-title="Action">
-									<div class="">
-										<ul class="list-inline justify-content-center">
-											<li class="list-inline-item">
-												<a data-toggle="tooltip" data-placement="top" title="Tooltip on top" class="view" href="">
-													<i class="fa fa-eye"></i>
-												</a>
-											</li>
-											<li class="list-inline-item">
-												<a class="edit" href="">
-													<i class="fa fa-pencil"></i>
-												</a>
-											</li>
-											<li class="list-inline-item">
-												<a class="delete" href="">
-													<i class="fa fa-trash"></i>
-												</a>
-											</li>
-										</ul>
-									</div>
-								</td> -->
-							</tr>
-            <?php }?>
-
-						</tbody>
-					</table>
-
+				<!-- Edit Personal Info -->
+				<div class="widget personal-info">
+          <h3 style="color: green"><?php echo $_SESSION['message']; unset($_SESSION['message']); ?></h3>
+					<h3 class="widget-header user">Edit Personal Information</h3>
+          <form action="lesseeProfile.php" method="POST">
+            <input type="hidden" name="editing" value="profile">						<!-- First Name -->
+						<div class="form-group">
+						    <label for="first-name">First Name</label>
+						    <input type="text" name="first-name" class="form-control" id="first-name" value="<?php echo $LesseeInfo[0]['FirstName']; ?>">
+						</div>
+						<!-- Last Name -->
+						<div class="form-group">
+						    <label for="last-name">Last Name</label>
+						    <input type="text" name="last-name" class="form-control" id="last-name" value="<?php echo $LesseeInfo[0]['LastName']; ?>">
+						</div>
+						<!-- Company -->
+						<div class="form-group">
+						    <label for="Company">Company</label>
+						    <input type="text" name="Company" class="form-control" id="Company" value="<?php echo $LesseeInfo[0]['Company']; ?>">
+						</div>
+						<!-- File chooser -->
+						<!-- <div class="form-group choose-file">
+							<i class="fa fa-user text-center"></i>
+						    <input type="file" class="form-control-file d-inline" id="input-file">
+						 </div> -->
+						<!-- Comunity Name -->
+						<!-- <div class="form-group">
+						    <label for="comunity-name">Comunity Name</label>
+						    <input type="text" class="form-control" id="comunity-name">
+						</div> -->
+						<!-- Checkbox -->
+						<!-- <div class="form-check">
+						  <label class="form-check-label" for="hide-profile">
+						    <input class="form-check-input" type="checkbox" value="" id="hide-profile">
+						    Hide Profile from Public/Comunity
+						  </label>
+						</div> -->
+						<!-- Phone Number -->
+						<div class="form-group">
+						    <label for="PhoneNumber">Phone Number</label>
+						    <input type="num" length="10" name="PhoneNumber" class="form-control" id="PhoneNumber" value="<?php echo $LesseeInfo[0]['PhoneNumber']; ?>">
+						</div>
+						<!-- Email -->
+						<div class="form-group">
+						    <label for="Email">Email</label>
+						    <input type="email" name="Email" class="form-control" id="Email" value="<?php echo $LesseeInfo[0]['Email']; ?>">
+						</div>
+						<!-- Submit button -->
+						<button class="btn btn-transparent">Save My Changes</button>
+					</form>
+				</div>
+				<!-- Change Password -->
+				<div class="widget change-password">
+					<h3 class="widget-header user">Edit Password</h3>
+					<form action="lesseeProfile.php" method="POST">
+            <input type="hidden" name="editing" value="password">
+						<!-- Current Password -->
+						<div class="form-group">
+              <input type="password" name="current-password" class="form-control" id="current-password">
+						</div>
+						<!-- New Password -->
+						<div class="form-group">
+						    <label for="new-password">New Password</label>
+						    <input type="password" name="new-password" class="form-control" id="new-password">
+						</div>
+						<!-- Submit Button -->
+						<button class="btn btn-transparent">Change Password</button>
+					</form>
 				</div>
 			</div>
 		</div>
-		<!-- Row End -->
 	</div>
-	<!-- Container End -->
 </section>
+
 <!--============================
 =            Footer            =
 =============================-->
