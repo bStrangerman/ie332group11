7e875c61b2a9b49e48edb6555285e4ad9c791617
Brandon Haberman <brandonhaberman@gmail.com>
2018-12-01 01:56:24 -0500

    owner user profile

--- layouts/sb_admin_2/header.php
+++ layouts/sb_admin_2/header.php
@@ -210,10 +210,10 @@
                           <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
                       </a>
                       <ul class="dropdown-menu dropdown-user">
-                          <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+                          <li><a href="ownerProfile.php"><i class="fa fa-user fa-fw"></i> User Profile</a>
                           </li>
-                          <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                          </li>
+                          <!-- <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+                          </li> -->
                           <li class="divider"></li>
                           <li><a href="../pages/logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
                           </li>
@@ -227,17 +227,6 @@
               <div class="navbar-default sidebar" role="navigation">
                   <div class="sidebar-nav navbar-collapse">
                       <ul class="nav" id="side-menu">
-                          <li class="sidebar-search">
-                              <div class="input-group custom-search-form">
-                                  <input type="text" class="form-control" placeholder="Search...">
-                                  <span class="input-group-btn">
-                                  <button class="btn btn-default" type="button">
-                                      <i class="fa fa-search"></i>
-                                  </button>
-                              </span>
-                              </div>
-                              <!-- /input-group -->
-                          </li>
                           <li>
                               <a href="warehouse.php"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                           </li>
